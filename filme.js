953ceab4cc57a27d78aaebcb43ebff9a3f0580ef
Pedro <pedro.l.oliveira@df.estudante.senai.br>
2023-08-20 15:23:58 -0300

    att

--- filme.js
+++ filme.js
@@ -1,53 +1,61 @@
 // 2) faça uma página de cadastramento de filmes onde o usuario possa salvar um filme e suas informações como nome, descrição,data, diretor e categoria e liste ela de alguma forma em algum elemento HTML!
 var inputFilme = document.querySelector("#nomeFilme");
-var inputDtLancamento = document.querySelector("#dtLancamento");
+var inputAnoLancamento = document.querySelector("#anoLancamento");
 var inputDiretor = document.querySelector("#diretor");
 var inputGenero = document.querySelector("#genero");
 var inputSinopse = document.querySelector("#sinopse");
 var btnCadastrar = document.querySelector("#btnCadastrar");
 
-var filmes = [];
+//Contrutor(mode) pré definido para criação de OBJETOS
 class Filme{
-    constructor(nomeFilme,dtLancamento,diretor,genero,sinopse){
+    constructor(nomeFilme,anoLancamento,diretor,genero,sinopse){
         this.nomeFilme = nomeFilme;
-        this.dtLancamento = dtLancamento;
+        this.anoLancamento = anoLancamento;
         this.diretor = diretor;
         this.genero = genero;
         this.sinopse = sinopse; 
     }
 }
 
-btnCadastrar.addEventListener("click",cadastrarFilme)
+var filmes = [
+    new Filme("Poderoso chefão","1972","Francis Ford Coppola","Drama","O patriarca idoso de uma dinastia do crime organizado transfere o controle de seu império clandestino para seu filho relutante."),
+    new Filme("O jogo da imitação","2014","Morten Tyldum","Drama","Durante a segunda guerra mundial, um inglês gênio da matemática tenta decifrar o código alemão Enigma com a ajuda de seus colegas."),
+    new Filme("Snowden: Herói ou Traidor","2016","Oliver Stone","Biografia","As técnicas ilegais da vigilância do NSA são filtradas ao público por um empregado deles, Edward Snowden, em forma de documentos classificados e dados a prensa.")
+];
 
-function cadastrarFilme(){
-    if(inputFilme.value == "" || inputFilme.value == undefined || inputFilme.value == null){
-        alert("Por favor digite o nome do filme");
-    }else if(inputDtLancamento.value == "" || inputDtLancamento == null || inputDtLancamento == undefined){
-        alert("Por favor digite a data de lancamento")
-    }else if(inputDiretor.value == "" || inputDiretor.value == undefined || inputDiretor.value == null){
-        alert("Por favor digite o nome do diretor");
-    }else if(inputGenero.value == "" || inputGenero.value == undefined || inputGenero.value == null){
-        alert("Por favor digite o gênero");
-    }else if(inputSinopse.value == "" || inputSinopse.value == undefined || inputSinopse.value == null){
-        alert("Por favor insira a sinopse"); 
+btnCadastrar.addEventListener("click",()=>{
 
-    }else{
-        filmes.push(new Filme(inputFilme.value,inputDtLancamento.value,inputDiretor.value,inputGenero.value,inputSinopse.value));
-        console.log(filmes);
-        alert("Filme cadastrado com sucesso!");
-
-        function apagarCampos(){
-            inputFilme.value = "";
-            inputDtLancamento.value = "";
-            inputDiretor.value = "";
-            inputGenero.value = "";
-            inputSinopse.value = "";
-        }
-
-        apagarCampos();
+        if(inputFilme.value == "" || inputFilme.value == undefined || inputFilme.value == null){
+            alert("Por favor digite o nome do filme");
+        }else if(inputAnoLancamento.value == "" || inputAnoLancamento == null || inputAnoLancamento == undefined){
+            alert("Por favor digite a data de lancamento")
+        }else if(inputDiretor.value == "" || inputDiretor.value == undefined || inputDiretor.value == null){
+            alert("Por favor digite o nome do diretor");
+        }else if(inputGenero.value == "" || inputGenero.value == undefined || inputGenero.value == null){
+            alert("Por favor digite o gênero");
+        }else if(inputSinopse.value == "" || inputSinopse.value == undefined || inputSinopse.value == null){
+            alert("Por favor insira a sinopse"); 
     
+        }else{
+            alert("Tem certeza que todas as informações estão corretas?")
+            filmes.push(new Filme(inputFilme.value,inputAnoLancamento.value,inputDiretor.value,inputGenero.value,inputSinopse.value));
+        
+            alert("Filme cadastrado com sucesso!");
+    
+            function apagarCampos(){
+                inputFilme.value = "";
+                inputAnoLancamento.value = "";
+                inputDiretor.value = "";
+                inputGenero.value = "";
+                inputSinopse.value = "";
+            }
+        apagarCampos();
+        
     }
-}
+})
+
+console.log(filmes);
+
 
 
 
